bf0e7f1febc8ea53121c47ed4562fe31e61389a9
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-06-18 14:50:52 -0300

    Adicionando logout à sidebar

--- src/components/layout/sidebar/SidebarFooter.tsx
+++ src/components/layout/sidebar/SidebarFooter.tsx
@@ -1,13 +1,30 @@
 
 import React from 'react';
 import { LogOut } from 'lucide-react';
-import { cn } from '@/lib/utils';
+import { useAuth } from '@/context/AuthContext';
+import { toast } from 'sonner';
+import { useNavigate } from 'react-router-dom';
 
 interface SidebarFooterProps {
   isCollapsed: boolean;
 }
 
 const SidebarFooter: React.FC<SidebarFooterProps> = ({ isCollapsed }) => {
+  const auth = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    try {
+      console.log('[DEBUG] Trying logout');
+      auth.logout();
+      console.log('[DEBUG] Success logout');
+      toast.info('Você desconectou da sua conta!')
+      navigate('/login');
+    } catch (error) {
+      toast.error(error);
+    }
+  }
+  
   return (
     <div className="p-3 border-t border-slate-200/60 bg-gradient-to-r from-white to-slate-50">
       <div className={cn(
@@ -16,7 +33,7 @@ const SidebarFooter: React.FC<SidebarFooterProps> = ({ isCollapsed }) => {
         isCollapsed ? "justify-center px-3" : "px-3"
       )}>
         <LogOut size={14} />
-        {!isCollapsed && <span className="font-medium text-xs">Sair</span>}
+        {!isCollapsed && <button onClick={handleLogout} className="font-medium text-xs">Sair</button>}
       </div>
     </div>
   );
