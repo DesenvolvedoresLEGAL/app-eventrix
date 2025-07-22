86592547d3638174a92c97bf71a855c5f442d441
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-22 17:39:54 -0300

    feat: drop triggers for updated_at columns
    This commit removes the triggers responsible for automatically updating the `updated_at` column in the `event_organizers`, `event_team`, and `events` tables. These triggers are no longer needed as the application logic will now handle updating these timestamps.

---
+++ supabase/migrations/20250722200726_drop_triggers.sql
@@ -0,0 +1,13 @@
+DROP TRIGGER IF EXISTS update_event_organizers_updated_at ON event_organizers;
+DROP TRIGGER IF EXISTS update_event_team_updated_at ON event_team;
+DROP TRIGGER IF EXISTS update_events_updated_at ON events;
+
+
+
+
+
+
+
+
+
+	
