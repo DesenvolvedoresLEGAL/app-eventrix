08bf1ac29b363a137ae1029e115a6234e55ed3e5
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-23 14:07:46 -0300

    Create 20250723170317_create_validate_cep.sql

---
+++ supabase/migrations/20250723170317_create_validate_cep.sql
@@ -0,0 +1,18 @@
+-- Migration: 20250723170317_create_validate_cep.sql
+-- Descrição: Cria a função para validar o formato e os dígitos de verificação do CEP
+-- Autor: Gustavo Mota
+-- Data: 23/07/2025
+
+BEGIN;
+CREATE OR REPLACE FUNCTION validate_cep(cep_input TEXT)
+RETURNS BOOLEAN AS $$
+BEGIN
+    RETURN cep_input IS NULL OR cep_input ~ '^\d{5}-\d{3}$';
+END;
+$$ LANGUAGE plpgsql;
+COMMIT;
+
+--ROLLBACK;
+-- BEGIN;
+-- DROP FUNCTION IF EXISTS validate_cep;
+--COMMIT;
