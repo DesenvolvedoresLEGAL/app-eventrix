29f6694bb8dbb7a53c4940d7a8acdcbb73f3a35b
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-23 17:36:58 -0300

    Create 20250723203255_create_policys_rls_on_organizer_types.sql

---
+++ supabase/migrations/20250723203255_create_policys_rls_on_organizer_types.sql
@@ -0,0 +1,15 @@
+-- Migration: 20250723203255_create_policys_rls_on_organizer_types.sql
+-- Descrição: Criação de políticas RLS para a tabela organizer_types
+-- Tabela: organizer_types
+-- Autor: Gustavo Mota
+-- Data: 23/07/2025
+
+BEGIN;
+CREATE POLICY "organizer_types_read_all" ON organizer_types
+    FOR SELECT
+    USING (auth.uid() IS NOT NULL);
+COMMIT;
+
+--ROLLBACK;
+-- DROP POLICY "organizer_types_read_all" ON organizer_types;
+-- COMMIT;
