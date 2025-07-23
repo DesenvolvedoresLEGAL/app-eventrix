0006c4dc5e766502e3c1c3d994efc3ab264b6f2e
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-23 09:07:31 -0300

    feat: create TenantRegisterExample page

---
+++ src/pages/TenantRegisterExample.tsx
@@ -0,0 +1,10 @@
+const TenantRegisterExample = () => {
+    return (
+        <div>
+            <h1>Tenant Register Example</h1>
+            <p>This is an example of a tenant register page.</p>
+        </div>
+    );
+}
+
+export default TenantRegisterExample;
