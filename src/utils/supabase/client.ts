f4ebfbacda9422596a2c75e2a4b82d5509652bbe
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-28 10:12:27 -0300

    Update client.ts

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,11 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY, {
-  auth: {
-    persistSession: true,
-    autoRefreshToken: true,
-  },
-});
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
 
 export default supabase;
