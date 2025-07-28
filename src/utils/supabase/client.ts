efb3d7dfae9e08dd50c08e4d0d1f8a9c4249f6f5
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-28 14:03:30 -0300

    Update client.ts

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -3,8 +3,8 @@ import { Database } from './types'; // Adjust the import path as necessary
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient<Database>(
-    import.meta.env.VITE_SUPABASE_URL,
-    import.meta.env.VITE_SUPABASE_ANON_KEY
+    'http://127.0.0.1:54321',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'
 );
 
 export default supabase;
