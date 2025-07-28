333763771b3bc17db4122ae8f04b281ce70bceb1
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-28 14:03:25 -0300

    Create Test.tsx

---
+++ src/pages/Test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Teste = () => (
+  <div className="min-h-screen flex items-center justify-center bg-muted">
+    <h1>Área de testes de desenvolvimento</h1>
+  </div>
+);
+
+export default Teste;
