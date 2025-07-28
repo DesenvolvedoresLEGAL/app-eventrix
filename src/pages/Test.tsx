f69ce5f13256a2d015e57e56b3ffd0fb9210a01c
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-28 14:34:50 -0300

    Update Test.tsx

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,8 +1,12 @@
+import CompanyBasicInfo from "@/features/Onboarding/pages/CompanyBasicInfo";
+import UserRegister from "@/features/Onboarding/pages/UserRegister";
 import React from "react";
 
 const Teste = () => (
-  <div className="min-h-screen flex items-center justify-center bg-muted">
+  <div className="min-h-screen gap-5 flex flex-col items-center justify-center bg-muted">
     <h1>Área de testes de desenvolvimento</h1>
+    <UserRegister />
+    <CompanyBasicInfo />
   </div>
 );
 
