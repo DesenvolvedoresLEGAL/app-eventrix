eae2d5f19f5d45862e59f713ee3284ccb20fcdfa
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-28 16:54:26 -0300

    Update Test.tsx

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,12 +1,14 @@
-import CompanyBasicInfo from "@/features/Onboarding/pages/CompanyBasicInfo";
-import UserRegister from "@/features/Onboarding/pages/UserRegister";
+import CompanyBasicInfo from "@/features/Onboarding/components/CompanyBasicInfoStep";
+import ContactInfoStep from "@/features/Onboarding/components/ContactInfoStep";
+import LocationStep from "@/features/Onboarding/components/LocationStep";
+import UserRegister from "@/features/Onboarding/components/UserRegisterStep";
+import EnterpriseOnboardWizard from "@/features/Onboarding/pages/EnterpriseOnboardWizard";
 import React from "react";
 
 const Teste = () => (
-  <div className="min-h-screen gap-5 flex flex-col items-center justify-center bg-muted">
+  <div className="min-h-screen gap-5 p-10 flex flex-col items-center justify-center bg-muted">
     <h1>Área de testes de desenvolvimento</h1>
-    <UserRegister />
-    <CompanyBasicInfo />
+    <EnterpriseOnboardWizard />
   </div>
 );
 
