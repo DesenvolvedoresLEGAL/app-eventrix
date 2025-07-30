ca6b7540409e605156a03c84a6df395d11d94c63
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-30 09:24:44 -0300

    Create roles.sql

---
+++ supabase/roles.sql
@@ -0,0 +1,16 @@
+
+SET default_transaction_read_only = off;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+
+CREATE ROLE "cli_login_postgres";
+ALTER ROLE "cli_login_postgres" WITH NOINHERIT NOCREATEROLE NOCREATEDB LOGIN NOBYPASSRLS VALID UNTIL '2025-07-30 12:06:46.003648+00';
+
+ALTER ROLE "anon" SET "statement_timeout" TO '3s';
+
+ALTER ROLE "authenticated" SET "statement_timeout" TO '8s';
+
+ALTER ROLE "authenticator" SET "statement_timeout" TO '8s';
+
+RESET ALL;
