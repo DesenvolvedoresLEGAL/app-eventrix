0d17d0805cda364b01bca44bed284fc2a674661b
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-30 15:19:04 -0300

    refactor: adding singou to sidebar

--- src/components/layout/sidebar/SidebarFooter.tsx
+++ src/components/layout/sidebar/SidebarFooter.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { LogOut } from 'lucide-react';
 import { cn } from '@/lib/utils';
+import supabase from '@/utils/supabase/client';
 
 interface SidebarFooterProps {
   isCollapsed: boolean;
@@ -10,7 +11,9 @@ interface SidebarFooterProps {
 const SidebarFooter: React.FC<SidebarFooterProps> = ({ isCollapsed }) => {
   return (
     <div className="p-3 border-t border-slate-200/60 bg-gradient-to-r from-white to-slate-50">
-      <div className={cn(
+      <div
+      onClick={async () => {await supabase.auth.signOut}}
+      className={cn(
         "flex items-center gap-2 px-3 py-2 rounded-xl cursor-pointer transition-all duration-300",
         "text-red-600 hover:text-red-700 hover:bg-red-50 hover:shadow-sm",
         isCollapsed ? "justify-center px-3" : "px-3"
