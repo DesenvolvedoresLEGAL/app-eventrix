abbc69e352b94e5a4ed93c75c7fb2524e77c2b8f
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-07-30 09:24:50 -0300

    Create 20250730121720_create_trigger_trigger_handle_auth_signup.sql

---
+++ supabase/migrations/20250730121720_create_trigger_trigger_handle_auth_signup.sql
@@ -0,0 +1,18 @@
+-- Migration: 20250730121720_create_trigger_trigger_handle_auth_signup.sql
+-- Table: profiles
+-- Description: Criar trigger em auth.users para executar a função que insere os dados de usuários em profiles.
+-- Author: Gustavo Mota
+-- Date: 30/07/2025
+
+BEGIN;
+DROP TRIGGER IF EXISTS trigger_handle_auth_signup ON auth.users;
+CREATE TRIGGER trigger_handle_auth_signup
+  BEFORE INSERT ON auth.users
+  FOR EACH ROW
+  EXECUTE FUNCTION public.handle_auth_signup();
+-- COMMIT;
+
+-- ROLLBACK;
+DROP TRIGGER IF EXISTS trigger_handle_auth_signup ON auth.users;
+-- DROP FUNCTION IF EXISTS public.handle_auth_signup();
+COMMIT;
