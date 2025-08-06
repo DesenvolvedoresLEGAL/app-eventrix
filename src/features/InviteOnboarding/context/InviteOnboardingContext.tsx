4f1a3cc69da29ccb4fe04f6b60ce62dfb40f536a
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-08-06 13:50:44 -0300

    test: add onboarding wizard happy path test

--- src/features/InviteOnboarding/context/InviteOnboardingContext.tsx
+++ src/features/InviteOnboarding/context/InviteOnboardingContext.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 import React, { createContext, useContext, useReducer, ReactNode, useMemo, useCallback } from 'react'
 import { InviteOnboardingState, InviteOnboardingAction } from '../types/invite.types'
