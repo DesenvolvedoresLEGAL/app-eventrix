9d358a42b6ca245d9f454940016c1c79c5baa6b9
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2025-08-08 08:35:16 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,7 @@ const App = () => (
         {/* TODO(auth-guard): Wrap all non-public routes with PrivateRoute/RouteGuard */}
         <Routes>
           {/* TODO(auth-guard): '/' is private; enforce auth via PrivateRoute */}
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<PrivateRoute><Index /></PrivateRoute>} />
           {/* TODO(auth-guard): Public route */}
           <Route path="/login" element={<Login />} />
           {/* TODO(auth-guard): Public route */}
